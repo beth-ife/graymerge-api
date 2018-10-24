1f2087e15030bbc670f491b0e7d6c18748a73c7d
Ifeoluwa Afolabi <ifeoluwa.afolabi@algorismng.com>
2018-10-24 19:04:18 +0100

    seeder files added

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.use('/products', require('./products'));
 router.use('/colors', require('./colors'));
 router.use('/currencies', require('./currencies'));
 router.use('/images', require('./images'));
+router.use('/upload-image',(req,res,next)=>{
+    return res.json(req.body)
+});
 
 
 module.exports = router;
