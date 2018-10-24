39bd048d9b2dc497ef49faa6c7a789c3ba15bdf8
Ifeoluwa Afolabi <ifeoluwa.afolabi@algorismng.com>
2018-10-24 18:20:51 +0100

    setting up cors

--- app.js
+++ app.js
@@ -26,7 +26,33 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(fileUpload());
+// setup CORS
+app.use((req, res, next) => {
+    // Websites you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, content-type, Authorization, token, Content-Type"
+    );
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    // Pass to next layer of middleware
+    next();
+});
 
+app.use((req, res, next) => {
+    if (req.method.toLowerCase() === "options") {
+        return res.status(204).send();
+    }
+    next();
+});
 
 //Handling file uploads at this point so I dont't have to deal with uploading every time a new endpoint requires file upload
 app.use(async (req, res, next) => {
